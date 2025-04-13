90be3dc561ae85ad2bc22506c5279a5532fe819b
Mark Harfouche <mark.harfouche@gmail.com>
2025-04-13 16:43:16 -0400

    Add more things to unify the builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,21 +3,17 @@ set -xeo pipefail
 
 rm -f subprojects/gtest.wrap
 EXTRA_MESON_ARGS=""
+EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dgstreamer=enabled"
+EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dtest=false"
+EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dcam=enabled"
+EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpycamera=enabled"
+if [[ ${target_platform} == "linux-*" ]]; then
+  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dv4l2=true"
+fi
 if [[ ${variant} == "rpi_fork" ]]; then
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpycamera=enabled"
   EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpipelines=rpi/vc4,rpi/pisp"
   EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dipas=rpi/vc4,rpi/pisp"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dv4l2=true"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dgstreamer=enabled"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dtest=false"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dlc-compliance=disabled"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dcam=disabled"
   EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dqcam=disabled"
-
-  # the latter two are needed to avoid build errors with GCC,
-  # see https://github.com/raspberrypi/libpisp/pull/43
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dc_args=-Wno-unused-parameter"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dcpp_args=-Wno-unused-parameter"
 fi
 
 meson setup build ${MESON_ARGS} \
@@ -25,4 +21,3 @@ meson setup build ${MESON_ARGS} \
      ${EXTRA_MESON_ARGS}
 
 ninja -C build install
-
