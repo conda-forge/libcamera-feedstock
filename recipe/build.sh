e7043b8f603be001e98422c1143c06e99ff45186
Mark Harfouche <mark.harfouche@gmail.com>
2025-04-13 18:20:01 -0400

    enable qcam on all

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,12 +6,12 @@ EXTRA_MESON_ARGS=""
 EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dgstreamer=enabled"
 EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dtest=false"
 EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dcam=enabled"
+EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dqcam=enabled"
 EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpycamera=enabled"
 EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dv4l2=true"
 if [[ ${variant} == "rpi_fork" ]]; then
   EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpipelines=rpi/vc4,rpi/pisp"
   EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dipas=rpi/vc4,rpi/pisp"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dqcam=disabled"
 fi
 
 meson setup build ${MESON_ARGS} \
