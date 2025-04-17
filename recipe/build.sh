8c25fbffbea652f8728a1bb1f544288fef5dede7
Mark Harfouche <mark.harfouche@gmail.com>
2025-04-17 07:13:52 -0400

    Try with imx8

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dv4l2=true"
 if [[ ${variant} == "rpi_fork" ]]; then
   # Make sure we build the standard pipeline and ipas contained in the upstream build
   # in ADDITION to the rpi/pisp ones
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpipelines=mx8-isi,mali-c55,rkisp1,rpi/vc4,simple,uvcvideo,rpi/pisp"
+  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpipelines=imx8-isi,mali-c55,rkisp1,rpi/vc4,simple,uvcvideo,rpi/pisp"
   EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dipas=mali-c55,rkisp1,rpi/vc4,simple,rpi/pisp"
 fi
 
