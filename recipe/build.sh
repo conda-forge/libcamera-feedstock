5c99368c082fe211a484d17b75e0248cc29fd560
Carlo <carlodri@users.noreply.github.com>
2025-04-17 08:01:22 +0200

    Update recipe/build.sh
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,10 @@ EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dqcam=enabled"
 EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpycamera=enabled"
 EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dv4l2=true"
 if [[ ${variant} == "rpi_fork" ]]; then
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpipelines=rpi/vc4,rpi/pisp"
-  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dipas=rpi/vc4,rpi/pisp"
+  # Make sure we build the standard pipeline and ipas contained in the upstream build
+  # in ADDITION to the rpi/pisp ones
+  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dpipelines=mx8-isi,mali-c55,rkisp1,rpi/vc4,simple,uvcvideo,rpi/pisp"
+  EXTRA_MESON_ARGS="${EXTRA_MESON_ARGS} -Dipas=mali-c55,rkisp1,rpi/vc4,simple,rpi/pisp"
 fi
 
 meson setup build ${MESON_ARGS} \
