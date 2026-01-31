629cf90eb5b929dbdee83e821c9bcf5333a649c6
Carlo <carlo.dri@gmail.com>
2026-01-31 18:56:38 +0100

    attempt a fix following @elephantum suggestions

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,3 +22,11 @@ meson setup build ${MESON_ARGS} \
      ${EXTRA_MESON_ARGS}
 
 ninja -C build install
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
